84f2fb58ca0c325f8ccbfcd626643ff80eca4828
Mohammed Ben Said <ices.dyers.0s@icloud.com>
2024-06-19 23:02:11 +0200

    Create CategoryRepository.cs

---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,29 @@
+public class CategoryRepository : ICategoryRepository
+{
+        public void Add(Category category)
+        {
+            // Implementation here
+        }
+
+        public void Update(Category category)
+        {
+            // Implementation here
+        }
+
+        public void Delete(int id)
+        {
+            // Implementation here
+        }
+
+        public Category GetById(int id)
+        {
+            // Implementation here
+            return null;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            // Implementation here
+            return null;
+        }
+}
